39ee4b337f2dfdd316ee3a2f02d40c987aea23c1
BlessedVictim <tequila1000@yandex.ru>
2019-01-05 19:06:42 +0300

    triggering test

--- app/utils/Helper.scala
+++ app/utils/Helper.scala
@@ -8,7 +8,7 @@ import pdi.jwt.JwtJson
 import play.api.libs.json.Json
 import services.ConfigService
 
-//Helper must help with buildbot triggering_9
+//Helper must help with buildbot triggering_test
 object Helper {
 
   implicit class DateTimeExtended(dt: DateTime) {
