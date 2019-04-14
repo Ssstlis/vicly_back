aadb555b773b3f84cdbcfa394b6a4aed0a997169
BlessedVictim <tequila1000@yandex.ru>
2019-04-14 16:38:29 +0300

    LOL UPD

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,5 +1,6 @@
 package models
 
+import io.swagger.annotations._
 import org.bson.types.ObjectId
 import org.joda.time.DateTime
 import play.api.libs.functional.syntax._
@@ -8,21 +9,24 @@ import salat.annotations.Key
 import utils.Helper.DateTimeExtended
 import utils.JsonHelper.ObjectIdFormat
 
+import scala.annotation.meta.field
+
+@ApiModel("User")
 case class User(
-  _id: ObjectId,
-  @Key("first_name") firstName: String,
-  @Key("last_name") lastName: String,
-  @Key("group_id") groupId: Option[Int],
-  password: String,
-  login: String,
-  active: Boolean,
-  @Key("join_time") joinTime: Int,
-  id: Int,
-  @Key("last_activity") lastActivity: Int = 0,
-  archive: Boolean = false,
-  avatar: String = "",
-  @Key("role_id") roleId: Int = 0
-) {
+                 @(ApiModelProperty@field) _id: ObjectId,
+                 @(ApiModelProperty@field) @Key("first_name") firstName: String,
+                 @(ApiModelProperty@field) @Key("last_name") lastName: String,
+                 @(ApiModelProperty@field) @Key("group_id") groupId: Option[Int],
+                 @(ApiModelProperty@field) password: String,
+                 @(ApiModelProperty@field) login: String,
+                 @(ApiModelProperty@field) active: Boolean,
+                 @(ApiModelProperty@field) @Key("join_time") joinTime: Int,
+                 @(ApiModelProperty@field) id: Int,
+                 @(ApiModelProperty@field) @Key("last_activity") lastActivity: Int = 0,
+                 @(ApiModelProperty@field) archive: Boolean = false,
+                 @(ApiModelProperty@field) avatar: String = "",
+                 @(ApiModelProperty@field) @Key("role_id") roleId: Int = 0
+               ) {
   def toJson = {
     Json.obj(
       "_id" -> _id,
@@ -59,19 +63,19 @@ trait UserJson {
 
   def reads(id: => Int): Reads[User] = (
     Reads.pure(new ObjectId()) and
-    ((__ \ "first_name").read[String] orElse Reads.pure("")) and
-    ((__ \ "last_name").read[String] orElse Reads.pure("")) and
-    (__ \ "group_id").readNullable[Int] and
-    (__ \ "password").read[String] and
-    (__ \ "login").read[String] and
-    Reads.pure(true) and
-    Reads.pure(DateTime.now.timestamp) and
-    Reads.pure(id) and
-    Reads.pure((System.currentTimeMillis() / 1000).toInt) and
-    Reads.pure(false) and
-    Reads.pure("") and
-    ((__ \ "role_id").read[Int] orElse Reads.pure(0))
-  )(User.apply _)
+      ((__ \ "first_name").read[String] orElse Reads.pure("")) and
+      ((__ \ "last_name").read[String] orElse Reads.pure("")) and
+      (__ \ "group_id").readNullable[Int] and
+      (__ \ "password").read[String] and
+      (__ \ "login").read[String] and
+      Reads.pure(true) and
+      Reads.pure(DateTime.now.timestamp) and
+      Reads.pure(id) and
+      Reads.pure((System.currentTimeMillis() / 1000).toInt) and
+      Reads.pure(false) and
+      Reads.pure("") and
+      ((__ \ "role_id").read[Int] orElse Reads.pure(0))
+    ) (User.apply _)
 }
 
 object User extends UserJson
