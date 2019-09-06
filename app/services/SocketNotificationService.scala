c9a92ebf383170c0bbfcd116be0cfbba9217ef89
blessedvictim <tequila1000@yandex.ru>
2019-09-06 21:20:21 +0300

    fix for WS notification of message changed event

--- app/services/SocketNotificationService.scala
+++ app/services/SocketNotificationService.scala
@@ -35,8 +35,8 @@ class SocketNotificationService @Inject()(subscriberService: SubscriberService) 
       (_: Int) => subscriberService.allSubscribers)
   }
 
-  def changed(groupId: Int, id: ObjectId, userIds: List[Int]) = {
-    push(3, groupId, Json.obj("id" -> id), userIds.contains,
+  def changed(groupId: Int, messageId: ObjectId, chat: Chat, userIds: List[Int]) = {
+    push(3, groupId, Json.obj("id" -> messageId, "chat" -> chat), userIds.contains,
       (_: Int) => subscriberService.allSubscribers)
   }
 
